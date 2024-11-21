5a0afbfa3c835c1b2f512b55ead97781818545b6
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2024-11-21 13:44:50 +0100

    Add options

--- File-search.py
+++ File-search.py
@@ -1,50 +1,59 @@
-def main():
+def start():
+    print ("welcome to the word searcher")
+    print ("Make sure that your file and this scri2 is in the same directory")
+    print ("Option 1: Tells you the cordinates of the word(s)")
+    print ("Option 2: shows you the amount of maches of the word(s)")
+    meny = input("choose option 1 or 2: ")
+    options(meny)
+
+def options(number):
+    if number == "1":
+        find_word_cordinates()
+    elif number == "2":
+        find_word_amount()
+    else:
+        print ("Invalid option. Please choose 1 or 2.")
+        start()
+    
+
+
+def find_word_cordinates():
     fname = input ("Enter a file name: ") 
     fopner = open(fname) # Brukeren beskriver en fil og python åpner filen
+    count = 0
+    line_number = 0
 
-    print ("Enter 1 if you want to find the line number")
-    print ("Enter 2 if you want to know the amount of matches")
-    UserInput = input("Enter a number: ")
-    if UserInput == "1":
-        LineNumber()
-    elif UserInput == "2":
-        MatchCount()
-    else:
-        print ("Invalid input. Please enter 1 or 2.")
-        main()
-    
-    def LineNumber():
-        count = 0
-        line_number = 0
-        content = input ("Enter the content you want to find: ")
-        for line in fopner: # for vær linje i filen
-            line_number += 1
-            words = line.split() # splitter alle ordene per linje
-            if content in line: # Når python finner ordet
-                ()
-                for word in words: # for alle ord i ord-linjen
-                    if word == content:
-                        count += 1
-                        print ("It is a",'"' + content + '"', "at line number", line_number, "at position", words.index(word), "from", fname)
-    def MatchCount():
-        count = 0
-        line_number = 0
-        content = input ("Enter the content you want to find: ")
-        for line in fopner: # for vær linje i filen
-            line_number += 1
-            words = line.split() # splitter alle ordene per linje
-            if content in line: # Når python finner ordet
-                ()
-                for word in words: # for alle ord i ord-linjen
-                    if word == content:
-                        count += 1
-                        print("There was", count, '"' + content + '"', "in the file")
-main()
+    content = input ("Enter the content you want to find: ")
+    for line in fopner: # for vær linje i filen
+        line_number += 1
+        words = line.split() # splitter alle ordene per linje
+        if content in line: # Når python finner ordet
+            ()
+            for word in words: # for alle ord i ord-linjen
+                if word == content:
+                    count += 1
+                    print ("It is a",'"' + content + '"', "at line number", line_number, "after", words.index(word) + 1, "words from the start line")
 
+def find_word_amount():
+    fname = input ("Enter a file name: ") 
+    fopner = open(fname)
+    count = 0
+    line_number = 0
+
+    content = input ("Enter the content you want to find: ")
+    for line in fopner:
+        line_number += 1
+        words = line.split()
+        if content in line:
+            ()
+            for word in words:
+                if word == content:
+                    count += 1
+    print ("There is", count,'"' + content + '"', "in this file")
+
+start()
 restart = input ("Do you want to seach again? (y/n): ")
 if restart == "y":
-    main()
+    start()
 else:
-    exit()
-
-main()
+    exit() # avslutter 
