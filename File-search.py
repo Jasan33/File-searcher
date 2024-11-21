8213253aaef7abf84ed4b13832eddd52938c5919
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2024-11-21 11:59:07 +0100

    Added more functions

--- File-search.py
+++ File-search.py
@@ -1,17 +1,50 @@
-fname = input ("Enter a file name: ") 
-fopner = open(fname) # Brukeren skirver en input fil og python åpner filen
-count = 0
-line_number = 0
+def main():
+    fname = input ("Enter a file name: ") 
+    fopner = open(fname) # Brukeren beskriver en fil og python åpner filen
 
-content = input ("Enter the content you want to find: ")
-for line in fopner:
-    line_number += 1
-    words = line.split()
-    if content in line: # Når linjen med det brukeren skrev matcher
-        count += 1
-        ()
-        for word in words:
-            if word == content:
-                print ("It is a",'"' + content + '"', "at line number", line_number, "at position", words.index(word) + 1, "from", fname)
-print("There was", count, '"' + content + '"', "in the file")
-input ("press enter to continue...")
+    print ("Enter 1 if you want to find the line number")
+    print ("Enter 2 if you want to know the amount of matches")
+    UserInput = input("Enter a number: ")
+    if UserInput == "1":
+        LineNumber()
+    elif UserInput == "2":
+        MatchCount()
+    else:
+        print ("Invalid input. Please enter 1 or 2.")
+        main()
+    
+    def LineNumber():
+        count = 0
+        line_number = 0
+        content = input ("Enter the content you want to find: ")
+        for line in fopner: # for vær linje i filen
+            line_number += 1
+            words = line.split() # splitter alle ordene per linje
+            if content in line: # Når python finner ordet
+                ()
+                for word in words: # for alle ord i ord-linjen
+                    if word == content:
+                        count += 1
+                        print ("It is a",'"' + content + '"', "at line number", line_number, "at position", words.index(word), "from", fname)
+    def MatchCount():
+        count = 0
+        line_number = 0
+        content = input ("Enter the content you want to find: ")
+        for line in fopner: # for vær linje i filen
+            line_number += 1
+            words = line.split() # splitter alle ordene per linje
+            if content in line: # Når python finner ordet
+                ()
+                for word in words: # for alle ord i ord-linjen
+                    if word == content:
+                        count += 1
+                        print("There was", count, '"' + content + '"', "in the file")
+main()
+
+restart = input ("Do you want to seach again? (y/n): ")
+if restart == "y":
+    main()
+else:
+    exit()
+
+main()
